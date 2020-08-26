067e006092cda1fab9efed8999097c98dceb3b0f
Enrique Hilst <enrique.hilst@gmail.com>
2020-08-26 16:32:30 -0500

    challenge 1 passing pre-interface

---
+++ Lab03.Tests/ChallengeTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Lab03;
+using Xunit;
+namespace Lab03Tests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 6)]
+        [InlineData(new[] { 3, 4, 5 }, 60)]
+        [InlineData(new[] { 7, 8 }, 0)]
+        public void Challenge1_multiplies_3_nums_in_array_only(int[] inputArray, int expected)
+        {
+            //Act
+            int result = Program.Challenge1(inputArray);
+
+            //Assert
+            Assert.Equal(result, expected);
+        }
+    }
+}
--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
 namespace Lab03
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("running");
         }
+
+        public static int Challenge1(int[] array)
+        {
+            if (array.Length < 3)
+                return 0;
+            else
+            {
+                int total = 1;
+                for (int i = 0; i < 3; i++)
+                {
+                    total *= array[i];
+                }
+                return total;
+            }
+
+        }
+
+        //public static int Challenge2(int input)
+        //{
+
+        //}
     }
 }
